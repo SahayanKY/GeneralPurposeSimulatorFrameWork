967575259b6ce7f3b9a486111504f72364d65bb0
KYuki <iclcor196@i.softbank.jp>
2018-06-04 18:50:51 +0900

    UnitEditorのテストクラスの作成

---
+++ src/icg/UnitEditorTest.java
@@ -0,0 +1,24 @@
+package icg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnitEditorTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMethod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConvert_from_to() {
+		fail("Not yet implemented");
+	}
+
+}
