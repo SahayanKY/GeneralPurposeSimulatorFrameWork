16dff73052d691b37d51028e1965c5e6288772d5
SahayanKY <iclcor196@i.softbank.jp>
2018-06-06 00:59:15 +0900

    javaの仕様のテスト

--- src/icgtest/DataInputFrameTest.java
+++ src/icgtest/DataInputFrameTest.java
@@ -1,16 +1,16 @@
 package icgtest;
 
-import icg.frame.DataInputFrame;
-
 public class DataInputFrameTest {
 	public static void main(String args[]) {
-		new DataInputFrame();
-		//new DataInputFrameTest().test();
+		//new DataInputFrame();
+		new DataInputFrameTest().test();
 		//new TestDataInputFrame().testB();あああ
 	}
 
 	private void test() {
-
+		String st = "ab";
+		String s[] = st.split("b");
+		System.out.println(s.length);
 	}
 
 }
