c2a80cff359614081d1758c979952a1f807c0f86
SahayanKY <iclcor196@i.softbank.jp>
2018-06-07 05:04:27 +0900

    特になし

--- src/icgtest/DataInputFrameTest.java
+++ src/icgtest/DataInputFrameTest.java
@@ -1,9 +1,11 @@
 package icgtest;
 
+import icg.frame.DataInputFrame;
+
 public class DataInputFrameTest {
 	public static void main(String args[]) {
-		//new DataInputFrame();
-		new DataInputFrameTest().test();
+		new DataInputFrame();
+		//new DataInputFrameTest().test();
 		//new TestDataInputFrame().testB();あああ
 	}
 
