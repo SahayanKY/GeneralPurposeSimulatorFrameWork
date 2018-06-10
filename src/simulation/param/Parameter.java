145291bb58288fbe90a0f9173093fc4ef1346f03
SahayanKY <iclcor196@i.softbank.jp>
2018-06-11 02:13:35 +0900

    Parameterの汎化はinterfaceだとstaticメソッドをうまく扱えないので、abstractclassを用いることに

--- src/simulation/param/Parameter.java
+++ src/simulation/param/Parameter.java
@@ -1,28 +1,17 @@
 package simulation.param;
 
-import simulation.param.checker.DefaultParameterChecker;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
 import simulation.param.checker.ParameterChecker;
 
-public class Parameter {
-	public final String parentLabel, childLabel;
+public abstract class Parameter {
+	protected static ArrayList<? extends Parameter> paramList = new ArrayList<>();
+
+	public final String parentLabel, childLabel, maxValue, minValue;
 	public final ParameterChecker checker;
 
-	protected final String maxValue,minValue;
 	protected String value;
-
-	private static final ParameterChecker defaultChecker = new DefaultParameterChecker();
-
-	Parameter(String parentLabel, String childLabel, ParameterChecker checker){
-		this(parentLabel, childLabel, null,null,checker);
-	}
-
-	Parameter(String parentLabel, String childLabel){
-		this(parentLabel, childLabel, null, null, defaultChecker);
-	}
-
-	Parameter(String parentLabel, String childLabel, String maxValue, String minValue){
-		this(parentLabel,childLabel,maxValue,minValue,defaultChecker);
-	}
 
 	Parameter(String parentLabel, String childLabel, String maxValue, String minValue, ParameterChecker checker){
 		this.parentLabel = parentLabel;
@@ -37,13 +26,38 @@ public class Parameter {
 	 * @param input 入力値のString表現
 	 * @return 0の場合は異常なし、1の場合は警告、2の場合はエラーで計算続行不可
 	 */
-	private final int checkFormatOf(String input) {
-		int message = checker.checkFormatOf(input, maxValue, minValue);
-		if(message == 0 || message == 1) {
-			value = input;
+	public abstract int checkFormatOf(String input) ;
+
+	/*
+	 * パラメータの配列を返す。
+	 * @return このパラメータ(列挙子)をすべて持つ配列
+	 * */
+	public static Parameter[] values() {
+		Parameter[] params = new Parameter[paramList.size()];
+		for(int i=0;i<params.length;i++) {
+			params[i] = paramList.get(i);
 		}
-		return message;
+		return params;
 	}
 
+	/*
+	 * パラメータの構造を示すマップを返す。
+	 * */
+	public static LinkedHashMap<String,LinkedHashMap<String,String>> getEnumMap(){
+		LinkedHashMap<String,LinkedHashMap<String,String>> parentMap = new LinkedHashMap<>();
+		for(Parameter param : Parameter.values()) {
+			String parentLabel = param.parentLabel;
+			String childLabel = param.childLabel;
+			LinkedHashMap<String,String> childMap = parentMap.getOrDefault(parentLabel, new LinkedHashMap<String,String>());
+			childMap.put(childLabel, param.value);
+			parentMap.put(parentLabel, childMap);
+		}
+		return parentMap;
+	}
+
+	/*
+	 * このパラメータが「燃焼データ」を表すものかどうかを判断する。
+	 * */
+	public abstract boolean isThrustDataParameter();
 }
 
