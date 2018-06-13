be9b4895246df66f2c359995b32d6da626810166
SahayanKY <iclcor196@i.softbank.jp>
2018-06-13 22:19:17 +0900

    Parameterは基本的に拡張するクラスでは無いと考え、Parameterのabstract宣言を除去

--- src/simulation/param/Parameter.java
+++ src/simulation/param/Parameter.java
@@ -1,23 +1,22 @@
 package simulation.param;
 
-import java.util.ArrayList;
-
 import simulation.param.checker.ParameterChecker;
 
-public abstract class Parameter {
-	protected static ArrayList<? extends Parameter> paramList = new ArrayList<>();
-
+public class Parameter {
 	public final String parentLabel, childLabel, maxValue, minValue;
 	public final ParameterChecker checker;
 
-	protected String value;
+	public final boolean isThrustDataParam;
 
-	Parameter(String parentLabel, String childLabel, String maxValue, String minValue, ParameterChecker checker){
+	private String value;
+
+	Parameter(String parentLabel, String childLabel, String maxValue, String minValue, boolean isThrustDataParam, ParameterChecker checker){
 		this.parentLabel = parentLabel;
 		this.childLabel = childLabel;
 		this.maxValue = maxValue;
 		this.minValue = minValue;
 		this.checker = checker;
+		this.isThrustDataParam = isThrustDataParam;
 	}
 
 	/*
@@ -25,7 +24,13 @@ public abstract class Parameter {
 	 * @param input 入力値のString表現
 	 * @return 0の場合は異常なし、1の場合は警告、2の場合はエラーで計算続行不可
 	 */
-	public abstract int checkFormatOf(String input) ;
+	public int checkFormatOf(String input) {
+		int message = checker.checkFormatOf(input, maxValue, minValue);
+		if(message == 0 || message == 1) {
+			value = input;
+		}
+		return message;
+	}
 
 	/*
 	 * このパラメータのもつ値を返す。
@@ -35,22 +40,18 @@ public abstract class Parameter {
 		return this.value;
 	}
 
-	/*
-	 * パラメータの配列を返す。
-	 * @return このパラメータ(列挙子)をすべて持つ配列
-	 * */
-	public static Parameter[] values() {
-		Parameter[] params = new Parameter[paramList.size()];
-		for(int i=0;i<params.length;i++) {
-			params[i] = paramList.get(i);
-		}
-		return params;
-	}
 
 
 	/*
 	 * このパラメータが「燃焼データ」を表すものかどうかを判断する。
+	 * @return このパラメータが「燃焼データ」であればtrue,そうでなければfalse
 	 * */
-	public abstract boolean isThrustDataParameter();
+	public boolean isThrustDataParameter() {
+		return this.isThrustDataParam;
+	}
+
+	public void setValue(String inputValue) {
+		this.value = inputValue;
+	}
 }
 
