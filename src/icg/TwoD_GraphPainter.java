008eaf49d85c8dc7557d33789493616feafec637
SahayanKY <iclcor196@i.softbank.jp>
2018-06-14 20:05:59 +0900

    試験的な実装

--- src/icg/TwoD_GraphPainter.java
+++ src/icg/TwoD_GraphPainter.java
@@ -4,10 +4,12 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class TwoD_GraphPainter implements GraphPainter {
+	int i=0;
 
 	public void graphPaint(Graphics g) {
 		g.setColor(Color.black);
-		g.drawLine(0, 0, 100, 100);
+		g.drawLine(0, i, 100, 100+i);
+		i += 10;
 	}
 
 	public void setData(double[][] data) {
