b2c09f5b84eb2478f613443f6b29e4ab14bffa1e
SahayanKY <iclcor196@i.softbank.jp>
2018-06-14 15:12:28 +0900

    PainterにGraphicsを渡して描画させることができるのを確認

--- src/icg/GraphPainter.java
+++ src/icg/GraphPainter.java
@@ -3,5 +3,7 @@ package icg;
 import java.awt.Graphics;
 
 public interface GraphPainter {
-	public void graphPaint(Graphics g) ;
+	public void graphPaint(Graphics g);
+	public void setData(double[][] data);
+	public void savePaint();
 }
