d8be44fed93db707b22aeba8c299b910f435c35e
SahayanKY <iclcor196@i.softbank.jp>
2018-06-15 18:19:07 +0900

    Frameの実装中

--- src/icg/GraphViewFrame.java
+++ src/icg/GraphViewFrame.java
@@ -1,21 +1,16 @@
 package icg;
 
-import java.awt.Graphics;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.IIOImage;
-import javax.imageio.ImageIO;
-import javax.imageio.ImageWriteParam;
-import javax.imageio.ImageWriter;
-import javax.imageio.stream.FileImageOutputStream;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import icg.frame.GraphLabel;
 
@@ -34,46 +29,70 @@ public class GraphViewFrame extends JFrame {
 	}
 
 	private GraphLabel graphLb;
+	private CardLayout LayoutOfPanel = new CardLayout();
+	private JPanel cardPanel = new JPanel();
 
 	private GraphPainter currentPainter;
 
-	public static void main(String[] args) {
-		new GraphViewFrame("GraphFrame");
+	public GraphViewFrame(String title){
+		setTitle(title);
+		setBounds(50,50,600,600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setComponent();
+		setVisible(true);
 	}
 
-	GraphViewFrame(String title){
-		setBounds(100,100,700,700);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	private void setComponent() {
+		ComponentSetter LayoutOfFrame = new ComponentSetter();
+		setLayout(LayoutOfFrame);
 
-		JPanel panel = new JPanel();
-
+		//カードレイアウトのパネルの追加
+		cardPanel.setPreferredSize(new Dimension(250,250));
+		LayoutOfFrame.setComponent(cardPanel, 0, 0, 1, 1, 0, 0, GridBagConstraints.CENTER);
+		add(cardPanel);
+		//グラフ(画像)を描画するラベルの追加
 		graphLb = new GraphLabel();
-		ImageIcon icon = new ImageIcon("D:/ゆうき/キャプチャ10.jpg");
-		graphLb.setIcon(icon);
 		setPainter(GraphKind.TwoD_Graph.painter);
+		graphLb.setOpaque(true);
+		graphLb.setBackground(Color.red);
 		graphLb.setReactiveToClick(true);
-		panel.add(graphLb);
+		graphLb.setPreferredSize(new Dimension(250,250));
+		LayoutOfFrame.setComponent(graphLb, 1, 0, 1, 1, 0, 1, GridBagConstraints.CENTER);
+		add(graphLb);
 
-		JButton paintBtn = new JButton("描画");
-		paintBtn.addActionListener(new ActionListener() {
+
+		cardPanel.setLayout(LayoutOfPanel);
+		ComponentSetter LayoutOfCardsPanel = new ComponentSetter();
+		//------------------------------------------------------------------------
+		//--------------------カードパネルその1-----------------------------------
+		//------------------------------------------------------------------------
+		JPanel startCard = new JPanel();
+		cardPanel.add(startCard, "card1");
+
+		JButton startDispertionGraphBtn = new JButton("落下分散の図を作成");
+		startDispertionGraphBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				LayoutOfPanel.show(cardPanel, "card2");
+				/*
 				graphLb.repaint();
+				*/
 			}
 		});
-		panel.add(paintBtn);
+		LayoutOfCardsPanel.setComponent(startDispertionGraphBtn, 0, 0, 1, 1, 0, 1, GridBagConstraints.CENTER);
+		startCard.add(startDispertionGraphBtn);
 
-		JButton savebtn = new JButton("保存");
-		savebtn.addActionListener(new ActionListener() {
+		JButton start2DGraphBtn = new JButton("数値データのグラフを作成");
+		start2DGraphBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				/*
 				try (FileImageOutputStream output = new FileImageOutputStream(new File("D:/ゆうき/AAA.jpg"))) {
 
 					BufferedImage readImage = ImageIO.read(new File("D:/ゆうき/キャプチャ10.jpg"));
 
 					Graphics graphics = readImage.createGraphics();
 
-					//	いたずら書き
 					currentPainter.graphPaint(graphics);
 
 					ImageWriter writeImage = ImageIO.getImageWritersByFormatName("jpeg").next();
@@ -86,22 +105,47 @@ public class GraphViewFrame extends JFrame {
 				} catch (IOException exc) {
 					exc.printStackTrace();
 				}
+				*/
 			}
 		});
-		panel.add(savebtn);
+		LayoutOfCardsPanel.setComponent(start2DGraphBtn, 0, 1, 1, 1, 0, 1, GridBagConstraints.CENTER);
+		startCard.add(start2DGraphBtn);
 
-		JButton changeBtn = new JButton("画像変更");
-		changeBtn.addActionListener(new ActionListener() {
+		JButton exitBtn = new JButton("終了");
+		exitBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		panel.add(changeBtn);
+		LayoutOfCardsPanel.setComponent(exitBtn, 0, 2, 1, 1, 0, 1, GridBagConstraints.CENTER);
+		startCard.add(exitBtn);
 
-		this.add(panel);
-		setVisible(true);
+
+		//------------------------------------------------------------------------
+		//--------------------カードパネルその1-----------------------------------
+		//------------------------------------------------------------------------
+		JPanel dispersion_setConfigCard = new JPanel();
+		cardPanel.add(dispersion_setConfigCard, "card2");
+
+		JTextField MapFilePathText = new JTextField();
+		MapFilePathText.setEditable(false);
+		LayoutOfCardsPanel.setFill(GridBagConstraints.HORIZONTAL);
+		LayoutOfCardsPanel.setComponent(MapFilePathText, 0, 0, 1, 1, 0, 0, GridBagConstraints.CENTER);
+		dispersion_setConfigCard.add(MapFilePathText);
+
+		JButton MapFileSelectBtn = new JButton("選択");
+		MapFileSelectBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		LayoutOfCardsPanel.setComponent(MapFileSelectBtn, 1, 0, 1, 1, 0, 0, GridBagConstraints.CENTER);
+		dispersion_setConfigCard.add(MapFileSelectBtn);
+
+
+
 	}
-
 
 	private void setPainter(GraphPainter painter) {
 		currentPainter = painter;
---
+++ src/icgtest/GraphViewFrameTest.java
@@ -0,0 +1,9 @@
+package icgtest;
+
+import icg.GraphViewFrame;
+
+public class GraphViewFrameTest {
+	public static void main(String[] args) {
+		new GraphViewFrame("GraphFrame");
+	}
+}
