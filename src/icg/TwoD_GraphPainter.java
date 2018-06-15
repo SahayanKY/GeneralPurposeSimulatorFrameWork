86fd58b4d9c60bffa953a63264781481c35d97fd
SahayanKY <iclcor196@i.softbank.jp>
2018-06-15 09:22:32 +0900

    以前の描画を残しつつの描画方法の確認

--- src/icg/TwoD_GraphPainter.java
+++ src/icg/TwoD_GraphPainter.java
@@ -2,13 +2,23 @@ package icg;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 public class TwoD_GraphPainter implements GraphPainter {
 	int i=0;
 
+	BufferedImage BI = new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB);
+
+
 	public void graphPaint(Graphics g) {
-		g.setColor(Color.black);
-		g.drawLine(0, i, 100, 100+i);
+		Graphics2D BufferedG = BI.createGraphics();
+
+		BufferedG.setColor(Color.black);
+		BufferedG.drawLine(0, i, 100, 100+i);
+
+		g.drawImage(BI, 0,0,null);
+
 		i += 10;
 	}
 
