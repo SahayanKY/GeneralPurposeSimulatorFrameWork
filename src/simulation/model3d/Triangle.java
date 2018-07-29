21151ff9e03474d1ef3859d36f2b346dc66f0d57
SahayanKY <iclcor196@i.softbank.jp>
2018-07-29 18:26:34 +0900

    桁落ち誤差が少なくなるように修正

--- src/simulation/model3d/Triangle.java
+++ src/simulation/model3d/Triangle.java
@@ -1,8 +1,8 @@
-package model3d;
+package simulation.model3d;
 
 public class Triangle {
-	float vertexs[][];
-	float normal[] = new float[3];
+	public float vertexs[][];
+	public float normal[] = new float[3];
 
 	public Triangle(float[] v1, float[] v2, float[] v3) {
 		if(v1.length != 3 || v2.length != 3 || v3.length != 3) {
@@ -28,13 +28,14 @@ public class Triangle {
 
 	private void calcurateNormal() {
 		//外積から面の法線ベクトルを計算する
-		normal[0] = (vertexs[1][1]-vertexs[0][1])*(vertexs[2][2]-vertexs[0][2]) -(vertexs[1][2]-vertexs[0][2])*(vertexs[2][1]-vertexs[0][1]);
-		normal[1] = (vertexs[1][2]-vertexs[0][2])*(vertexs[2][0]-vertexs[0][0]) -(vertexs[1][0]-vertexs[0][0])*(vertexs[2][2]-vertexs[0][2]);
-		normal[2] = (vertexs[1][0]-vertexs[0][0])*(vertexs[2][1]-vertexs[0][1]) -(vertexs[1][1]-vertexs[0][1])*(vertexs[2][0]-vertexs[0][0]);
+		normal[0] = vertexs[0][1]*vertexs[1][2] +vertexs[1][1]*vertexs[2][2] +vertexs[2][1]*vertexs[0][2] -vertexs[0][1]*vertexs[2][2] -vertexs[1][1]*vertexs[0][2] -vertexs[2][1]*vertexs[1][2];
+		normal[1] = vertexs[0][2]*vertexs[1][0] +vertexs[1][2]*vertexs[2][0] +vertexs[2][2]*vertexs[0][0] -vertexs[0][2]*vertexs[2][0] -vertexs[1][2]*vertexs[0][0] -vertexs[2][2]*vertexs[1][0];
+		normal[2] = vertexs[0][0]*vertexs[1][1] +vertexs[1][0]*vertexs[2][1] +vertexs[2][0]*vertexs[0][1] -vertexs[0][0]*vertexs[2][1] -vertexs[1][0]*vertexs[0][1] -vertexs[2][0]*vertexs[1][1];
 		float normalNorm = (float) Math.sqrt(normal[0]*normal[0] +normal[1]*normal[1] +normal[2]*normal[2]);
 		//正規化
 		normal[0] /= normalNorm;
 		normal[1] /= normalNorm;
 		normal[2] /= normalNorm;
 	}
+
 }
