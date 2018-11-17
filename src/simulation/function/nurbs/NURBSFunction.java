95a4d2047275addb28e06c4c361e9684ba5b01bd
SahayanKY <iclcor196@i.softbank.jp>
2018-11-18 01:33:33 +0900

    これまでのテストコードを踏まえた例外処理の実装予定をコメントした

--- src/simulation/function/nurbs/NURBSFunction.java
+++ src/simulation/function/nurbs/NURBSFunction.java
@@ -12,18 +12,34 @@ public class NURBSFunction {
 	 * P_{0,0,...,0,n_{m-1}-1}はctrl[n_{m-1}-1]に格納される。
 	 * P_{0,0,...,1,0}はctrl[n_{m-1}]に格納される。
 	 * P_{i_0,i_1,...,i_{m-2},i_{m-1}}はctrl[i_{m-1}+i_{m-2}*n_{m-1}+...+i_0*n_1*...*n_{m-1}]に格納される。
-	 * これを前提として計算を行います。ctrlの第2のインデックスはコントロールポイント
-	 * がベクトル値だった場合にその成分をもつのに利用します。
+	 * これを前提として計算を行います。ctrlの第2のインデックスは各コントロールポイントの重みと座標
+	 * を保持します。ただしこのうち、1番目の要素は重みであり、正の数を必ず指定してください。
+	 * ここで全て1を指定した場合、Bスプラインに対応します。2番目以降がポイントの座標になります。
+	 * 最低でも2番目の要素を指定してください。
 	 *
 	 * @param ctrl コントロールポイントを指定する。
 	 * @param ctrlNum 各方向のコントロールポイントの数を指定する。
-	 * @param property このNURBS関数が必要とするノットベクトルを表すNURBSProperty
+	 * @param pro このNURBS関数が必要とするノットベクトルを表すNURBSProperty
 	 */
-	public NURBSFunction(double[][] ctrl, int[] ctrlNum, NURBSProperty property) {
-		//次数は設定させないのか？
-		//ctrlの要素数はproperty.knotとつじつまがあうのか
+	public NURBSFunction(double[][] ctrl, int[] ctrlNum, NURBSProperty pro) {
+		//ctrlの要素数はpro.knotとpro.pとつじつまがあうのか
+		for(int i=0;i<pro.parameterNum;i++) {
+			if(pro.knot[i].length != ctrlNum[i]+pro.p[i]+1) {
+				throw new IllegalArgumentException(
+					"コントロールポイントの数とノットベクトルの要素数と次数のつじつまが合いません"+
+					":pro.knot["+i+"].length != ctrlNum["+i+"]+pro.p["+i+"]+1"
+				);
+			}
+		}
+
+		//ctrlの各要素の成分の数は一定になっているのか、
+		//重み1+次元dになっているのか
+		//d!=0ではないか
+		//重みw>0か
+
+
 		this.ctrl = ctrl;
-		this.property = property;
+		this.property = pro;
 	}
 
 	/**
@@ -33,6 +49,8 @@ public class NURBSFunction {
 	 */
 	public double[] value(double... t){
 		//t.lengthはproperty.parameterNumと一致するのか
+		//tはNURBS関数の定義域に反していないか
+		//tの要素の内、それに対応するノットの最後端に等しかった場合の特別な対応
 		return null;
 	}
 
--- src/simulation/function/nurbs/NURBSProperty.java
+++ src/simulation/function/nurbs/NURBSProperty.java
@@ -3,7 +3,10 @@ package simulation.function.nurbs;
 import java.util.ArrayList;
 
 public class NURBSProperty {
+	/**各変数の基底関数のノットベクトル*/
 	protected double[][] knot;
+	/**各変数の基底関数の次数*/
+	protected int[] p;
 	//パラメータの数
 	protected final int parameterNum;
 	private final ArrayList<NURBSFunction> nurbslist = new ArrayList<>();
@@ -14,9 +17,26 @@ public class NURBSProperty {
 	 * 与え、knot.lengthは1であること。2変数の場合、knot[0]とknot[1]にそれぞれのノットベクトル
 	 * を与え、knot.lengthは2であること。以下同様である。
 	 */
-	public NURBSProperty(double[][] knot){
-		this.knot = knot;
+	public NURBSProperty(double[][] knot, int[] p){
+		//例外処理、パラメータチェック
+		if(knot.length != p.length) {
+			//変数の数が一致していない場合
+			throw new IllegalArgumentException("knotとpが示す変数の数が一致していません");
+		}
 		this.parameterNum = knot.length;
+
+		for(int i=0;i<parameterNum;i++) {
+			//各変数のノットベクトルは単調増加列になっているのか
+			for(int j=0;j<knot[i].length-1;j++) {
+				if(knot[i][j]>knot[i][j+1]) {
+					throw new IllegalArgumentException("ノットベクトルが単調増加列でありません:knot["+i+"]["+j+"]>knot["+i+"]["+j+1+"]");
+				}
+			}
+		}
+
+
+		this.knot = knot;
+		this.p = p;
 	}
 
 	/**
