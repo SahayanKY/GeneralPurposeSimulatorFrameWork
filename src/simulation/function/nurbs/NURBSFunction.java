1f13cb79062933e8e18cc657bad4fd3209e68204
SahayanKY <iclcor196@i.softbank.jp>
2018-11-17 18:44:38 +0900

    k法の実装を見据えて必要なメソッド及びjavadocの追加

--- src/simulation/function/nurbs/NURBSFunction.java
+++ src/simulation/function/nurbs/NURBSFunction.java
@@ -1,18 +1,45 @@
 package simulation.function.nurbs;
 
 public class NURBSFunction {
-	private final double[][] ctrl;
+	private double[][] ctrl;
 	private final NURBSProperty property;
 
-	public NURBSFunction(double[][] ctrl, NURBSProperty property) {
+	/**
+	 * NURBS関数をインスタンス化させます。
+	 * コントロールポイントは多変数NURBSの場合注意が必要です。
+	 * mを変数の数、Pをコントロールポイント、n_iを変数i方向のポイントの数としたとき、
+	 * P_{0,0,...,0,0}はctrl[0]に格納される。
+	 * P_{0,0,...,0,n_{m-1}-1}はctrl[n_{m-1}-1]に格納される。
+	 * P_{0,0,...,1,0}はctrl[n_{m-1}]に格納される。
+	 * P_{i_0,i_1,...,i_{m-2},i_{m-1}}はctrl[i_{m-1}+i_{m-2}*n_{m-1}+...+i_0*n_1*...*n_{m-1}]に格納される。
+	 * これを前提として計算を行います。ctrlの第2のインデックスはコントロールポイント
+	 * がベクトル値だった場合にその成分をもつのに利用します。
+	 *
+	 * @param ctrl コントロールポイントを指定する。
+	 * @param ctrlNum 各方向のコントロールポイントの数を指定する。
+	 * @param property このNURBS関数が必要とするノットベクトルを表すNURBSProperty
+	 */
+	public NURBSFunction(double[][] ctrl, int[] ctrlNum, NURBSProperty property) {
 		//次数は設定させないのか？
 		//ctrlの要素数はproperty.knotとつじつまがあうのか
 		this.ctrl = ctrl;
 		this.property = property;
 	}
 
+	/**
+	 * 変数値を引数で指定し、その点でのNURBS関数の値を計算します。
+	 * @param t 変数値
+	 * @return 関数値
+	 */
 	public double[] value(double... t){
 		//t.lengthはproperty.parameterNumと一致するのか
 		return null;
 	}
+
+	/**
+	 * k法によるコントロールポイントの変更を行います。
+	 * ただし、このメソッドは登録してあるNURBSPropertyインスタンスから呼び出される
+	 * ものであり、利用者は明示的に呼び出さないでください。
+	 */
+	protected void kmethod() {}
 }
--- src/simulation/function/nurbs/NURBSProperty.java
+++ src/simulation/function/nurbs/NURBSProperty.java
@@ -1,12 +1,39 @@
 package simulation.function.nurbs;
 
+import java.util.ArrayList;
+
 public class NURBSProperty {
-	protected final double[][] knot;
+	protected double[][] knot;
 	//パラメータの数
 	protected final int parameterNum;
+	private final ArrayList<NURBSFunction> nurbslist = new ArrayList<>();
 
-	public NURBSProperty(double[][] knot, int parameterNum){
+	/**
+	 * NURBSのノットベクトルを保有するNURBSPropertyをインスタンス化します。
+	 * @param knot ノットベクトルを指定する。1変数NURBSの場合、knot[0]にノットベクトルを
+	 * 与え、knot.lengthは1であること。2変数の場合、knot[0]とknot[1]にそれぞれのノットベクトル
+	 * を与え、knot.lengthは2であること。以下同様である。
+	 */
+	public NURBSProperty(double[][] knot){
 		this.knot = knot;
-		this.parameterNum = parameterNum;
+		this.parameterNum = knot.length;
 	}
+
+	/**
+	 * NURBSFunctionインスタンスをこのインスタンスに登録します。
+	 * k法によって精細化を行う際、NURBSFunctionの状態を変える必要があり、
+	 * それを一括で行うためのものです。なお、これはNURBSFunction内部から
+	 * 自動的に呼び出されるので、NURBSFunctionインスタンスを利用する際に
+	 * 明示的に呼び出してはなりません。
+	 * @param function
+	 */
+	protected void registerNURBSFunction(NURBSFunction function) {
+		this.nurbslist.add(function);
+	}
+
+	/**
+	 * k法によるノットの挿入、及び登録されているNURBSFunctionのコントロールポイントの
+	 * 追加を行います。
+	 * */
+	public void k_method(){}
 }
