f55d33a97cdd8b918b6863a3aa2a98118512f1a2
SahayanKY <iclcor196@i.softbank.jp>
2018-11-18 12:07:12 +0900

    予定していた例外処理を全て実装した

--- src/simulation/function/nurbs/NURBSFunction.java
+++ src/simulation/function/nurbs/NURBSFunction.java
@@ -2,7 +2,7 @@ package simulation.function.nurbs;
 
 public class NURBSFunction {
 	private double[][] ctrl;
-	private final NURBSProperty property;
+	private final NURBSProperty pro;
 
 	/**
 	 * NURBS関数をインスタンス化させます。
@@ -22,24 +22,84 @@ public class NURBSFunction {
 	 * @param pro このNURBS関数が必要とするノットベクトルを表すNURBSProperty
 	 */
 	public NURBSFunction(double[][] ctrl, int[] ctrlNum, NURBSProperty pro) {
-		//ctrlの要素数はpro.knotとpro.pとつじつまがあうのか
+		//nullチェック
+		if(ctrl == null) {
+			throw new IllegalArgumentException("引数ctrlがnullです");
+		}else if(ctrlNum == null) {
+			throw new IllegalArgumentException("引数ctrlNumがnullです");
+		}else if(pro == null) {
+			throw new IllegalArgumentException("引数proがnullです");
+		}
+
+		//コントロールポイントの数が1以上になっているのか
+		if(ctrl.length == 0) {
+			throw new IllegalArgumentException("引数ctrlの要素数が0です");
+		}
+
+		//変数の数が一致しているか
+		if(ctrlNum.length != pro.parameterNum) {
+			throw new IllegalArgumentException("関数の変数の数の指定がctrlNumとproとで一致していません");
+		}
+
+
+		int sumctrl = 1;
 		for(int i=0;i<pro.parameterNum;i++) {
+			//ctrlNumの各要素数は1以上になっているのか
+			if(ctrlNum[i]<1) {
+				throw new IllegalArgumentException("ctrlNum["+i+"]が1以上ではありません");
+			}
+
+			//コントロールポイントの数(ctrlNum)はpro.knotとpro.pとつじつまがあうのか
 			if(pro.knot[i].length != ctrlNum[i]+pro.p[i]+1) {
 				throw new IllegalArgumentException(
 					"コントロールポイントの数とノットベクトルの要素数と次数のつじつまが合いません"+
 					":pro.knot["+i+"].length != ctrlNum["+i+"]+pro.p["+i+"]+1"
 				);
 			}
+
+			sumctrl *= ctrlNum[i];
+		}
+
+
+		/*
+		 * ctrl.lengthはctrlNumの総積になっているのか
+		 * 例えばP_{0,0,0}からP_{2,2,2}まで存在する時、ctrl.length==27
+		 * ctrlNum[0]==3,ctrlNum[1]==3,ctrlNum[2]==3である
+		 * sumctrl == ctrlNum[0]*...*ctrlNum[2]となっている
+		*/
+		if(sumctrl != ctrl.length) {
+			throw new IllegalArgumentException("ctrlとctrlNumが示す全コントロールポイントの数が一致しません");
 		}
 
 		//ctrlの各要素の成分の数は一定になっているのか、
-		//重み1+次元dになっているのか
+		//重み1+次元d==Lになっているのか
 		//d!=0ではないか
 		//重みw>0か
+		int L=-1;
+		for(int i=0;i<ctrl.length;i++) {
+			double[] monoctrl=ctrl[i];
+			if(L==-1) {
+				L = monoctrl.length;
 
+				if(L < 2) {
+					throw new IllegalArgumentException("コントロールポイントの要素数が足りません:重み1+次元d(>0)");
+				}
+			}
+
+			if(L != monoctrl.length) {
+				throw new IllegalArgumentException("コントロールポイントctrl["+i+"]に次元数の過不足があります");
+			}
+
+			//先に重みを各座標に掛け合わせておく
+			for(int j=1;j<L;j++) {
+				monoctrl[j] *= monoctrl[0];
+			}
+		}
+
+		pro.registerNURBSFunction(this);
 
 		this.ctrl = ctrl;
-		this.property = pro;
+		this.pro = pro;
 	}
 
 	/**
@@ -48,9 +108,37 @@ public class NURBSFunction {
 	 * @return 関数値
 	 */
 	public double[] value(double... t){
-		//t.lengthはproperty.parameterNumと一致するのか
+		//指定された変数の数は想定している変数の数に一致しているか
+		if(t.length != pro.parameterNum) {
+			throw new IllegalArgumentException("変数の数が要求される数"+pro.parameterNum+"に合いません:"+t.length);
+		}
+
 		//tはNURBS関数の定義域に反していないか
-		//tの要素の内、それに対応するノットの最後端に等しかった場合の特別な対応
+		for(int i=0;i<pro.parameterNum;i++) {
+			//各変数について対応のノットベクトルの範囲の中にあるかを調べる
+			if(t[i] < pro.knot[i][0] || t[i] > pro.knot[i][pro.knot[i].length-1]) {
+				throw new IllegalArgumentException("指定された変数値t["+i+"]はノットベクトルの範囲を超えています");
+			}
+		}
+
+		//各変数についてt_k <= t < t_k+1となるようなkをさがす
+		int[] k = new int[pro.parameterNum];
+		for(int i=0;i<pro.parameterNum;i++) {
+			for(int j=0; j<pro.knot[i].length; j++) {
+				if(t[i] < pro.knot[i][j]) {
+					k[i] = j-1; //直前の位置がk
+					break;
+				}
+			}
+			//tがノットの最後端に等しい時、（値が違う）一つ前のノットを指定する
+			if(t[i] == pro.knot[i][pro.knot[i].length-1]) {
+				k[i] = pro.knot[i].length-pro.p[i]-2; //==n-1
+			}
+		}
+
+		//以降deBoorアルゴリズムの通り
+
+
 		return null;
 	}
 
--- src/simulation/function/nurbs/NURBSProperty.java
+++ src/simulation/function/nurbs/NURBSProperty.java
@@ -7,18 +7,26 @@ public class NURBSProperty {
 	protected double[][] knot;
 	/**各変数の基底関数の次数*/
 	protected int[] p;
-	//パラメータの数
+	/**変数の数*/
 	protected final int parameterNum;
+	/**registerNURBSFunction(NURBSFunction)で登録されたNURBSFunction*/
 	private final ArrayList<NURBSFunction> nurbslist = new ArrayList<>();
 
 	/**
-	 * NURBSのノットベクトルを保有するNURBSPropertyをインスタンス化します。
+	 * NURBSの基底関数組を保有するNURBSPropertyをインスタンス化します。
+	 * 指定するノットベクトルはオープンノットベクトルであることを前提とします。
 	 * @param knot ノットベクトルを指定する。1変数NURBSの場合、knot[0]にノットベクトルを
 	 * 与え、knot.lengthは1であること。2変数の場合、knot[0]とknot[1]にそれぞれのノットベクトル
 	 * を与え、knot.lengthは2であること。以下同様である。
+	 * @param p 各変数の基底関数の次数
 	 */
 	public NURBSProperty(double[][] knot, int[] p){
-		//例外処理、パラメータチェック
+		if(knot == null) {
+			throw new IllegalArgumentException("引数knotが指定されていません");
+		}else if(p == null) {
+			throw new IllegalArgumentException("引数pが指定されていません");
+		}
+
 		if(knot.length != p.length) {
 			//変数の数が一致していない場合
 			throw new IllegalArgumentException("knotとpが示す変数の数が一致していません");
@@ -26,8 +34,26 @@ public class NURBSProperty {
 		this.parameterNum = knot.length;
 
 		for(int i=0;i<parameterNum;i++) {
-			//各変数のノットベクトルは単調増加列になっているのか
+			//各変数の基底関数の次数は1以上になっているのか
+			if(p[i]<1) {
+				throw new IllegalArgumentException("次数p["+i+"]が1以上でありません");
+			}
+
+			//各ノットベクトルについて
 			for(int j=0;j<knot[i].length-1;j++) {
+				//オープンノットベクトルになっているか
+				//前後ろの要素がp+1個重なっているか
+				//0,1,...,pが同じ、n,...,n+p-1,n+pが同じ(n+p+1 == knot[i].length)
+				if(j<p[i] //前のノットについて調べるとき
+						||
+					knot[i].length-p[i]-1 <= j //後ろのノットについて調べるとき
+				) {
+					if(knot[i][j]!=knot[i][j+1]) {
+						throw new IllegalArgumentException("ノットベクトルknot["+i+"]がオープンノットベクトルでありません");
+					}
+				}
+
+				//各変数のノットベクトルは単調増加列になっているのか
 				if(knot[i][j]>knot[i][j+1]) {
 					throw new IllegalArgumentException("ノットベクトルが単調増加列でありません:knot["+i+"]["+j+"]>knot["+i+"]["+j+1+"]");
 				}
@@ -45,7 +71,7 @@ public class NURBSProperty {
 	 * それを一括で行うためのものです。なお、これはNURBSFunction内部から
 	 * 自動的に呼び出されるので、NURBSFunctionインスタンスを利用する際に
 	 * 明示的に呼び出してはなりません。
-	 * @param function
+	 * @param function 登録されるNURBSFunctionインスタンス
 	 */
 	protected void registerNURBSFunction(NURBSFunction function) {
 		this.nurbslist.add(function);
