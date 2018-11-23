a961af67203944bb830e07dcc91a5e9761e0518f
SahayanKY <iclcor196@i.softbank.jp>
2018-11-23 12:05:20 +0900

    アルゴリズムの流れを複数のメソッドに分割し、計算専門のクラスを作成

---
+++ src/simulation/function/nurbs/NURBSCalculater.java
@@ -0,0 +1,187 @@
+package simulation.function.nurbs;
+
+public abstract class NURBSCalculater {
+
+	/**
+	 * 各変数についてt_k <= t < t_k+1となるようなkを探します。
+	 * @param pro プロパティ
+	 * @param t 変数値
+	 * */
+	protected static int[] restrictKnotVectorRange(NURBSProperty pro, double[] t) {
+		int[] k = new int[pro.parameterNum];
+		for(int i=0;i<pro.parameterNum;i++) {
+			for(int j=0; j<pro.knot[i].length; j++) {
+				if(t[i] < pro.knot[i][j]) {
+					k[i] = j-1; //直前の位置がk
+					break;
+				}
+			}
+			//tがノットの最後端に等しい時、（値が違う）一つ前のノットを指定する
+			if(t[i] == pro.knot[i][pro.knot[i].length-1]) {
+				k[i] = pro.knot[i].length-pro.p[i]-2; //==n-1
+			}
+		}
+
+		return k;
+	}
+
+
+	/**
+	 * 計算に有効なコントロールポイントを返します。
+	 * 返される配列の具体的な意味は、
+	 * <ul>
+	 * 	<li>第1インデックスはコントロールポイントの指定
+	 * 	<li>第2インデックスはコントロールポイントの重み、重み*座標1、...
+	 * </ul>
+	 * です。funcにnullを指定した場合、第2インデックスは重みだけが返されます。
+	 * この配列の長さはpro.effCtrlNumです。
+	 *
+	 * @param k ノットの有効範囲パラメータ
+	 * @param pro プロパティ
+	 * @param func NURBSFunctionインスタンス
+	 * */
+	protected static double[][] restrictControlPoint(int[] k, NURBSProperty pro, NURBSFunction func){
+		int dimension;
+		if(func == null) {
+			dimension = 0;
+		}else {
+			dimension = func.ctrl[0].length;
+		}
+
+		double[][] Q = new double[pro.effCtrlNum][dimension+1];
+
+		//元のコントロールポイントから必要なものをコピーし初期化する
+		int[] indexs = new int[pro.parameterNum];
+		out:while(true) {
+			int Qindex=0,Pindex=0;
+			//i0,i1,...,i{m-1}というインデックスを1つの数に置き換える
+			for(int i=0;i<pro.parameterNum;i++) {
+				Qindex += indexs[i] *pro.Pi_p[i+1];
+				Pindex += (k[i]-pro.p[i]+indexs[i]) *pro.Pi_n[i+1];
+			}
+
+			Q[Qindex][0] = pro.weight[Qindex];
+			for(int i=1;i<dimension+1;i++) {
+				Q[Qindex][i] = func.ctrl[Pindex][i-1];
+			}
+
+			//繰り上がり処理
+			for(int i=indexs.length-1;i>=0;i--) {
+				indexs[i]++;
+				if(indexs[i]<=pro.p[i]) {
+					break;
+				}else {
+					indexs[i]=0;
+					if(i==0) {
+						//全ての組み合わせについて終了
+						break out;
+					}else {
+						continue;
+					}
+				}
+			}
+		}
+
+		return Q;
+	}
+
+
+	/**
+	 * deBoorのアルゴリズムのループ部分。
+	 *
+	 * これにより、f{i,j,..,k}N{i,p}N{j,q}...N{k,r}を計算したことになる。
+	 *
+	 * ノット範囲の限定、及びそれに基づくコントロールポイントの限定を行ってから、
+	 * このメソッドを呼び出す。
+	 * @param t 変数値
+	 * @param k ノット範囲の限定パラメータ。
+	 * @param Q 限定後のコントロールポイント。BスプラインのdeBoorアルゴリズムを
+	 * 作用させるものを指定する。
+	 * @param pro プロパティ
+	 * */
+	protected static double[] deBoorsLoop(double[] t, int[] k, double[][] Q, NURBSProperty pro) {
+		//4つループの入れ子
+		for(int l=pro.parameterNum-1;l>=0;l--) {
+			for(int r=0;r<=pro.p[l]-1;r++) {
+				for(int i=pro.p[l];i>=r+1;i--) {
+					double alpha
+						= (t[l] -pro.knot[l][i+k[l]-pro.p[l]])
+							/(pro.knot[l][i+k[l]-r] -pro.knot[l][i+k[l]-pro.p[l]]);
+
+
+					//0,0,...,0からp0,p1,...,p{l-1}まで繰り返す
+					int[] indexs = new int[pro.parameterNum];
+					//indexsのインデックスl+1からm-1までは次数pで固定であり、
+					//インデックスlは対象外（他の意味によって指定される）
+					for(int j=l+1;j<pro.parameterNum;j++) {
+						indexs[j] = pro.p[j];
+					}
+					indexs[l] = i;
+
+					out:while(true) {
+						//i{0},i{1},...,i{l-1},i{l},p{l+1},...,p{m-1}を変換したものを格納
+						int convertIndex = 0;
+						for(int j=0;j<pro.parameterNum;j++) {
+							convertIndex += indexs[j]*pro.Pi_p[j+1];
+						}
+
+						//deBoorの計算Q = (1-a)Q +aQの部分
+						for(int d=0;d<Q[0].length;d++) {
+							//コントロールポイントの各成分毎に計算
+							Q[convertIndex][d] =
+								(1-alpha)*Q[convertIndex-pro.Pi_p[l+1]][d]
+									+
+								alpha*Q[convertIndex][d];
+						}
+
+
+						//繰り上がり処理
+						//i0,i1,...,i{l-1}までを弄るのでl-1始まり
+						for(int j=l-1;j>=0;j--) {
+							indexs[j]++;
+							if(indexs[j]<=pro.p[j]) {
+								break;
+							}else {
+								indexs[j]=0;
+								if(j==0) {
+									//全ての組み合わせについて終了
+									break out;
+								}else {
+									continue;
+								}
+							}
+						}
+
+						if(l==0) {
+							break out;
+						}
+					}
+				}
+			}
+		}
+
+		//結果を格納しているp{0},p[1],...,p{m-1}はQ.length-1に等しい
+		return Q[Q.length-1];
+	}
+
+	/**
+	 * 重み*座標値となっている値を座標値単体に変換します。
+	 * 引数pointの第1インデックスには重みを、第2インデックス以降には
+	 * 重み*座標値を指定してください。また、戻り値は
+	 * 座標値1、座標値2、...となり、配列の長さは引数pointの長さの-1になります。
+	 * @param point 重み、重み*座標値1、...となっているポイント
+	 * @throws IllegalArgumentException pointの長さが1以下で処理できない場合。
+	 * */
+	protected static double[] processWeight(double[] point) {
+		if(point.length <= 1) {
+			throw new IllegalArgumentException("指定された配列の長さは1以下で処理できません");
+		}
+		int dimension = point.length -1;
+		double[] result = new double[dimension];
+		for(int i=0;i<dimension;i++) {
+			result[i] = point[i+1]/point[0];
+		}
+		return result;
+	}
+
+}
--- src/simulation/function/nurbs/NURBSFunction.java
+++ src/simulation/function/nurbs/NURBSFunction.java
@@ -35,7 +35,7 @@ public class NURBSFunction {
 
 
 	/**コントロールポイント。具体的な中身はコンストラクタを参照*/
-	private final double[][] ctrl;
+	protected final double[][] ctrl;
 
 	/**このインスタンスが必要とする基底関数のノットベクトルや次数*/
 	private final NURBSProperty pro;
@@ -109,163 +109,23 @@ public class NURBSFunction {
 		pro.checkVariableIsValid(t);
 
 		//各変数についてt_k <= t < t_k+1となるようなkをさがす
-		int[] k = new int[pro.parameterNum];
-		for(int i=0;i<pro.parameterNum;i++) {
-			for(int j=0; j<pro.knot[i].length; j++) {
-				if(t[i] < pro.knot[i][j]) {
-					k[i] = j-1; //直前の位置がk
-					break;
-				}
-			}
-			//tがノットの最後端に等しい時、（値が違う）一つ前のノットを指定する
-			if(t[i] == pro.knot[i][pro.knot[i].length-1]) {
-				k[i] = pro.knot[i].length-pro.p[i]-2; //==n-1
-			}
-		}
+		int[] k = NURBSCalculater.restrictKnotVectorRange(pro, t);
 
 		//以降deBoorアルゴリズムの通り
 		//Q[][0]:重み
 		//Q[][1]以降:重み*座標値
-		double Q[][] = restrictControlPoint(k, pro);
+		double Q[][] = NURBSCalculater.restrictControlPoint(k, pro, this);
 
 		//4つの入れ子ループ部分へ
-		double[] loopResult = this.deBoorsLoop(t, k, Q, pro);
+		//loopResult[0]:重みの足し合わせ結果
+		//loopResult[1]以降:重み*座標値の足し合わせ結果
+		double[] loopResult = NURBSCalculater.deBoorsLoop(t, k, Q, pro);
 
 		/*Q[resultIndex]には{重みの足し合わせ、座標1*重みの足し合わせ、...}が入っているため、
 		 * インデックス0で残りの要素を割り、その残りの要素を結果として出さなければならない
 		 * (NURBSの特徴)
 		 * */
-		double[] result = new double[dimension];
-		for(int i=1;i<=dimension;i++) {
-			result[i-1] = loopResult[i]/loopResult[0];
-		}
-
-		return result;
-	}
-
-
-	private double[][] restrictControlPoint(int[] k,NURBSProperty pro){
-//------------------------------------------------------------------------------------------
-		//dimensionの定義、一般化を考えた時に大丈夫か
-
-		double[][] Q = new double[pro.effCtrlNum][dimension+1];
-
-		//元のコントロールポイントから必要なものをコピーし初期化する
-		int[] indexs = new int[pro.parameterNum];
-		out:while(true) {
-			int Qindex=0,Pindex=0;
-			//i0,i1,...,i{m-1}というインデックスを1つの数に置き換える
-			for(int i=0;i<pro.parameterNum;i++) {
-				Qindex += indexs[i] *pro.Pi_p[i+1];
-				Pindex += (k[i]-pro.p[i]+indexs[i]) *pro.Pi_n[i+1];
-			}
-
-			Q[Qindex][0] = pro.weight[Qindex];
-			for(int i=1;i<dimension+1;i++) {
-				Q[Qindex][i] = ctrl[Pindex][i-1];
-			}
-
-			//繰り上がり処理
-			for(int i=indexs.length-1;i>=0;i--) {
-				indexs[i]++;
-				if(indexs[i]<=pro.p[i]) {
-					break;
-				}else {
-					indexs[i]=0;
-					if(i==0) {
-						//全ての組み合わせについて終了
-						break out;
-					}else {
-						continue;
-					}
-				}
-			}
-		}
-		return null;
-	}
-
-
-
-	/**
-	 * deBoorのアルゴリズムのループ部分。
-	 *
-	 * これにより、f{i,j,..,k}N{i,p}N{j,q}...N{k,r}を計算したことになる。
-	 *
-	 * ノット範囲の限定、及びそれに基づくコントロールポイントの限定を行ってから、
-	 * このメソッドを呼び出す。
-	 * @param t 変数値
-	 * @param k ノット範囲の限定パラメータ。
-	 * @param Q 限定後のコントロールポイント。BスプラインのdeBoorアルゴリズムを
-	 * 作用させるものを指定する。
-	 * @param pro プロパティ
-	 * */
-	private double[] deBoorsLoop(double[] t, int[] k, double[][] Q, NURBSProperty pro) {
-		//4つループの入れ子
-		for(int l=pro.parameterNum-1;l>=0;l--) {
-			for(int r=0;r<=pro.p[l]-1;r++) {
-				for(int i=pro.p[l];i>=r+1;i--) {
-					double alpha
-						= (t[l] -pro.knot[l][i+k[l]-pro.p[l]])
-							/(pro.knot[l][i+k[l]-r] -pro.knot[l][i+k[l]-pro.p[l]]);
-
-
-					//0,0,...,0からp0,p1,...,p{l-1}まで繰り返す
-					int[] indexs = new int[pro.parameterNum];
-					//indexsのインデックスl+1からm-1までは次数pで固定であり、
-					//インデックスlは対象外（他の意味によって指定される）
-					for(int j=l+1;j<pro.parameterNum;j++) {
-						indexs[j] = pro.p[j];
-					}
-					indexs[l] = i;
-
-					out:while(true) {
-						//i{0},i{1},...,i{l-1},i{l},p{l+1},...,p{m-1}を変換したものを格納
-						int convertIndex = 0;
-						for(int j=0;j<pro.parameterNum;j++) {
-							convertIndex += indexs[j]*pro.Pi_p[j+1];
-						}
-
-						//deBoorの計算Q = (1-a)Q +aQの部分
-						for(int d=0;d<Q[0].length;d++) {
-							//コントロールポイントの各成分毎に計算
-							Q[convertIndex][d] =
-								(1-alpha)*Q[convertIndex-pro.Pi_p[l+1]][d]
-									+
-								alpha*Q[convertIndex][d];
-						}
-
-
-						//繰り上がり処理
-						//i0,i1,...,i{l-1}までを弄るのでl-1始まり
-						for(int j=l-1;j>=0;j--) {
-							indexs[j]++;
-							if(indexs[j]<=pro.p[j]) {
-								break;
-							}else {
-								indexs[j]=0;
-								if(j==0) {
-									//全ての組み合わせについて終了
-									break out;
-								}else {
-									continue;
-								}
-							}
-						}
-
-						if(l==0) {
-							break out;
-						}
-					}
-				}
-			}
-		}
-
-		//結果を格納しているp{0},p[1],...,p{m-1}を変換し、取得する
-		int resultIndex=0;
-		for(int i=0;i<pro.parameterNum;i++) {
-			resultIndex += pro.p[i]*pro.Pi_p[i+1];
-		}
-		return Q[resultIndex];
+		return NURBSCalculater.processWeight(loopResult);
 	}
 
 	/**
