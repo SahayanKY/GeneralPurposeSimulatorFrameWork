a8f550413c81194ce44072fc799747c3c340e882
SahayanKY <iclcor196@i.softbank.jp>
2019-02-20 01:22:21 +0900

    複数のNURBSFunctionをまとめて精細化する実装が完了、プリミティブの配列レベルでの精細化実装はまだ

--- src/simulation/function/nurbs/NURBSRefiner.java
+++ src/simulation/function/nurbs/NURBSRefiner.java
@@ -1,13 +1,12 @@
 package simulation.function.nurbs;
 
-import java.util.ArrayList;
-
 public class NURBSRefiner {
 
 	/**
 	 * 指定されたNURBS基底関数に対して、ノットを挿入します。
-	 * 返されるインスタンスは指定されたものとは異なる参照をもちます。
+	 * 返されるインスタンスは指定されたものとは全く異なる参照をもちます。
 	 * また、返されるNURBSFunctionインスタンスの順番は、指定された順番と対応しています。
+	 * また、指定されたNURBS関数の状態は変化しません。
 	 *
 	 * @param group NURBS基底関数とコントロールポイントの組
 	 * @param X 各変数方向のノットベクトルに挿入するノット配列
@@ -24,24 +23,96 @@ public class NURBSRefiner {
 		//Xが正常かどうかを調べる
 		assertInsertedKnotVectorIsValid(group.basis,X);
 
+		NURBSBasisFunction basis = group.basis;
+		NURBSFunction[] funcs = group.funcs;
 
-		//最後に新しくNURBSFunctionインスタンスを作る時に必要となる
-		//各Functionのポイントの次元を把握しておく
-		ArrayList<Integer> pointDimension = new ArrayList<>();
-		pointDimension.add(1);
-		for(NURBSFunction f:group.func) {
-			pointDimension.add(f.dimension);
+		//元のコントロールポイント数
+		int n_All = group.basis.Pi_n[0];
+		//新しいコントロールポイントの数
+		int Newn_All = 1;
+		for(int varNum=0;varNum<basis.parameterNum;varNum++) {
+			//X[varNum].length:ある変数方向のポイントの増加数
+			Newn_All *= (basis.n[varNum]+X[varNum].length);
 		}
 
+		//---------------------------------------------------------------------
 
+		//これから操作するポイントの総座標数を計算する
+		int sumDimension=1;
+		for(NURBSFunction f:funcs) {
+			sumDimension += f.dimension;
+		}
 
+		//---------------------------------------------------------------------
 
+		//ctrl:今のポイントの値をまとめたもの
+		//NewCtrl:新しいポイントを保持するもの
+		//[][0]:重み
+		//[][1]:func[0]のポイントの1つめの座標...
+		double[][] ctrl = new double[n_All][sumDimension];
+		double[][] NewCtrl = new double[Newn_All][sumDimension];
 
-		return null;
+		for(int i=0;i<n_All;i++) {
+			ctrl[i][0] = basis.weight[i];
+		}
+		for(int i_func=0, j=1;i_func<funcs.length;i_func++) {
+			for(int d=0;d<funcs[i_func].dimension;d++,j++) {
+				for(int i=0;i<n_All;i++) {
+					ctrl[i][j] = funcs[i_func].ctrl[i][d];
+				}
+			}
+		}
+
+		//---------------------------------------------------------------------
+
+		//NewKnot:新しいノットを保持するもの
+		double[][] NewKnot = new double[basis.parameterNum][];
+		for(int varNum=0;varNum<basis.parameterNum;varNum++) {
+			NewKnot[varNum] = new double[basis.knot[varNum].length +X[varNum].length];
+		}
+
+		//---------------------------------------------------------------------
+
+		//新しいポイントとノットを計算する
+		refineKnot(ctrl,NewCtrl,basis.knot,X,NewKnot,basis.p);
+
+		//---------------------------------------------------------------------
+
+		//計算結果から、新しいNURBSBasisFunctionとNURBSFunctionを生成するために、
+		//それらのコンストラクタの引数に必要なものを用意する
+		int[] Newp = basis.p.clone();
+		double[] NewWeight = new double[Newn_All];
+
+		//各NURBSFunctionのポイント毎にまとめたもの
+		double[][][] NewCtrlEachFunc = new double[funcs.length][Newn_All][];
+		for(int i=0;i<Newn_All;i++) {
+			NewWeight[i] = NewCtrl[i][0];
+		}
+		for(int i_func=0, j=1;i_func<funcs.length;i_func++) {
+			for(int d=0;d<funcs[i_func].dimension;d++,j++) {
+				for(int i=0;i<n_All;i++) {
+					NewCtrlEachFunc[i_func][i][d] = NewCtrl[i][j];
+
+					//ctrl[i][j] = funcs[i_func].ctrl[i][d];
+				}
+			}
+		}
+
+		//---------------------------------------------------------------------
+		//NURBSBasisFunctionとNURBSFunctionをインスタンス化し、Groupにまとめて返す
+		NURBSBasisFunction NewBasis = new NURBSBasisFunction(NewKnot, Newp, NewWeight);
+		NURBSFunction[] NewFuncs = new NURBSFunction[funcs.length];
+		for(int i_func=0;i_func<funcs.length;i_func++) {
+			NewFuncs[i_func] = new NURBSFunction(NewCtrlEachFunc[i_func], NewBasis);
+		}
+
+		return new NURBSFunctionGroup(NewBasis, NewFuncs);
 	}
+
 
 	/**
 	 * ノットを精細化します。
+	 * ctrlとknotとX、そしてpの状態は変えません。
 	 *
 	 * @param ctrl 基底関数の重み、各NURBS関数のコントロールポイントをまとめたもの
 	 * @param NewCtrl 新しいポイントを保存する配列
@@ -49,6 +120,8 @@ public class NURBSRefiner {
 	 * @param X 挿入するノット
 	 * @param NewKnot 新しいノットベクトルを保存する配列
 	 * @param p 基底関数の次数
+	 *
+	 * @throws IllegalArgumentException ノットを挿入した事により関数の不連続化が起こる場合
 	 * */
 	private void refineKnot(double[][] ctrl, double[][] NewCtrl, double[][] knot, double[][] X, double[][] NewKnot, int[] p) {
 
