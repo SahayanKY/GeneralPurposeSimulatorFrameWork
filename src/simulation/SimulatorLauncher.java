df71ebe208e12cb047e519198e0343edc780dd55
SahayanKY <iclcor196@i.softbank.jp>
2019-11-02 19:57:15 +0900

    javadocの追加

--- src/simulation/SimulatorLauncher.java
+++ src/simulation/SimulatorLauncher.java
@@ -9,26 +9,55 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 
+/**
+ * シミュレーションを非同期に立ち上げ、実行させるクラス。
+ * */
 public class SimulatorLauncher {
+
+	/**実行するシミュレーションのクラス*/
 	private Simulator simulator;
+	/**立ち上げたマルチスレッド全部の完了をもって完了するCompletableFuture*/
 	private CompletableFuture<Void> allcf;
+	/**interfacer*/
 	private SimulatorGUIInterfacer interfacer;
 
-
+	/**
+	 * コンストラクタ。
+	 * @param simulator
+	 * 起動させるSimulatorクラスインスタンス
+	 * @param interfacer
+	 * GUI側とやり取りするSimulatorGUIInterfacerクラスインスタンス
+	 * */
 	SimulatorLauncher(Simulator simulator, SimulatorGUIInterfacer interfacer) {
 		this.simulator = simulator;
 		this.interfacer = interfacer;
 	}
 
+	/**
+	 * 呼び出すことによって、シミュレーションを完全に停止させます。
+	 * 途中でシミュレーションをやめる場合に利用してください。
+	 * これを呼び出した際に、ある単一条件のシミュレーションが実行中であった場合、
+	 * その条件が完了しファイル出力も完了した後に、
+	 * 次の条件に移行せずに停止します。
+	 * */
 	public void cancel() {
 		this.allcf.cancel(true);
 	}
 
+	/**
+	 * すべての単一条件が完了するまで、このメソッドを呼び出したスレッドを停止します。
+	 * 完了後は実行を再開します。
+	 * */
 	public void join() {
 		this.allcf.join();
 	}
 
 
+	/**
+	 * Simulatorを起動させます。
+	 * 並列数1であっても、これを呼び出したスレッドとは異なるスレッド上でシミュレーションは実行されます。
+	 * これを呼び出す前にsimulatorのパラメータの設定等は済ませておいてください。
+	 * */
 	public void launch() {
 		final ExecutorService es_each = Executors.newFixedThreadPool(simulator.parallelNum);
 
@@ -42,6 +71,9 @@ public class SimulatorLauncher {
 		this.allcf = this.getIntegratedCompletableFuture(cflist, es_each);
 	}
 
+	/**
+	 * すべてのTaskが完了したのをもって完了とするCompletableFutureを返します。
+	 * */
 	private CompletableFuture<Void> getIntegratedCompletableFuture(List<CompletableFuture<Void>> cflist, ExecutorService es) {
 		CompletableFuture<Void> allcf = CompletableFuture.allOf(
 					cflist.toArray(new CompletableFuture[cflist.size()])
@@ -62,7 +94,10 @@ public class SimulatorLauncher {
 		return allcf;
 	}
 
-
+	/**
+	 * 指定されていた並列数の数だけ、
+	 * 単一条件の計算を次々と実行していくCompletableFutureを生成します。
+	 * */
 	private List<CompletableFuture<Void>> executeMultithread(ExecutorService es) {
 		List<CompletableFuture<Void>> cflist = new ArrayList<>();
 
